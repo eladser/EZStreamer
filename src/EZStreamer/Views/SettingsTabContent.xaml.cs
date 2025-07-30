8971f1e5e52956ecf7f6625649265dc64fdf2a6b
eladser <54028171+eladser@users.noreply.github.com>
2025-07-30 16:05:23 +0300

    Add code-behind file for SettingsTabContent.xaml

---
+++ src/EZStreamer/Views/SettingsTabContent.xaml.cs
@@ -0,0 +1,76 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EZStreamer.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsTabContent.xaml
+    /// </summary>
+    public partial class SettingsTabContent : UserControl
+    {
+        public SettingsTabContent()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectTwitch_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement Twitch connection logic
+        }
+
+        private void ConnectSpotify_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement Spotify connection logic
+        }
+
+        private void ConnectYouTube_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement YouTube connection logic
+        }
+
+        private void PreferredMusicSource_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            // TODO: Implement music source preference change logic
+        }
+
+        private void ConnectOBS_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement OBS connection logic
+        }
+
+        private void TestOBSConnection_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement OBS connection test logic
+        }
+
+        private void OverlayTheme_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            // TODO: Implement overlay theme change logic
+        }
+
+        private void PreviewOverlay_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement overlay preview logic
+        }
+
+        private void OpenOverlayFolder_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement overlay folder opening logic
+        }
+
+        private void ResetSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement settings reset logic
+        }
+
+        private void ExportSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement settings export logic
+        }
+
+        private void ImportSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Implement settings import logic
+        }
+    }
+}
